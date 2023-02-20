32ced8b6262846ee41d3d9bc30c3c3e071aae738
Zhaoyi Yan <priscillayouziqian@gmail.com>
2023-02-19 22:03:28 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button, Form, Checkbox, DatePicker, Input, Select } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+const App = () => (
+  <div className="App">
+    <header className='App-header'>
+      <Form autoComplete='off' labelCol={{span: 10}} wrapperCol={{span:14}} 
+      onFinish={(values)=>{
+        console.log({values});
+      }}
+      onFinishFailed={(error)=>{
+        console.log({error});
+      }}
+      >
+        <Form.Item name="fullName" label="Full Name" rules={[
+          {
+            required:true,
+            message:"Please enter your name",
+          },
+          {
+            whitespace:true
+          },
+          {min:3},
+        
+        ]}
+        hasFeedback
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <Input placeholder='Type your name'/>
+        </Form.Item>
+
+        <Form.Item name="email" label="Email" rules={[
+          {
+            required:true,
+            message:"Please enter your email",
+          },
+          {type:"email", message:"Please enter a valid email"},
+        
+        ]}
+        hasFeedback
+        >
+          <Input placeholder='Type your email'/>
+        </Form.Item>
+
+        <Form.Item name="password" label="Password" rules={[
+          {
+            required:true,
+          },
+          {min: 6},
+          {
+            validator:(_,value) => 
+              value && value.includes('A') ? Promise.resolve() : Promise.reject('Password does not match criteria')
+            
+          },
+        ]}
+        hasFeedback
+        >
+          <Input.Password placeholder='Type your password'/>
+        </Form.Item>
+
+        <Form.Item name="confirmPassword" label="Confirm Password" 
+        dependencies={['password']} //refers to 1st enter password
+        rules={[
+          {
+            required:true,
+          },
+          ({getFieldValue})=>({
+            validator(_,value){
+              if(!value || getFieldValue('password') === value){
+                return Promise.resolve()
+              }
+              return Promise.reject('Two passwords you entered do not match')
+            }
+          })
+        ]}
+        hasFeedback
+        >
+          <Input.Password placeholder='Confirm your password'/>
+        </Form.Item>
+
+        <Form.Item name="gender" label="Gender" requiredMark="optional">
+          <Select placeholder='Select your gender'>
+            <Select.Option value="male">Male</Select.Option>
+            <Select.Option value="female">Female</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item name="dob" label="Date of Birth" rules={[
+          {
+            required:true,
+            message:"Please provide your date of birth",
+          },
+        ]}
+        hasFeedback>
+          <DatePicker style={{width:'100%'}} picker='date' placeholder='Choose your date of birth'/>
+        </Form.Item>
+
+        <Form.Item name="website" label="Website" rules={[
+          {type:"url", message:"Please enter a valid url"},
+        
+        ]}
+        hasFeedback>
+          <Input placeholder='Add your website url'/>
+        </Form.Item>
+
+        <Form.Item name="agreement" wrapperCol={{span: 24}} valuePropName="checked"
+        rules={[
+          {
+            validator:(_,value) => 
+              value ? Promise.resolve() : Promise.reject('To proceed, you need to agree with our terms and conditions'),
+            
+          },
+        
+        ]}>
+        
+          <Checkbox>Agree to our <a href='https://google.com'>Terms and Conditions</a></Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{span: 24}}>
+          <Button block type='primary' htmlType='submit'>Register</Button>
+        </Form.Item>
+      </Form>
+    </header>
+  </div>
+);
 
 export default App;
